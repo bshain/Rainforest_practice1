d989a99909805499e4d3614ead7f0f4cc5cf0087
bshain <shainbarbara@gmail.com>
2014-02-04 12:50:12 -0500

    Adding flash and alert notices

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
       render "new"
     end
   end
+
   private
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation, :name)
